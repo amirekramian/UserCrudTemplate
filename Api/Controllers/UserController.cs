2204c2cf332b33585f138620fd55f6431d055c38
Amir Ekramian <Amir Ekramian>
2022-05-26 00:10:40 +0430

    last commit of day

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+    }
+}

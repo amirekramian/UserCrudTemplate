fef6ee6f351563bbbf3d77002d47cda31289fecc
Amir Ekramian <Amir Ekramian>
2022-05-25 22:59:13 +0430

    add entity

---
+++ Core/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public  class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public string Tel { get; set; }
+        public string BirthDate { get; set; }
+        public string nationalCode { get; set; }
+    }
+}
